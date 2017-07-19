060e9250583d41f02f645a8d63bdd2db3bda1b56
fekefe <fekefe@126.com>
2017-07-19 17:51:33 +0800

    二叉树

--- app-algorithm/src/main/java/com/tree/basic/Node.java
+++ app-algorithm/src/main/java/com/tree/basic/Node.java
@@ -1,7 +1,10 @@
 package com.tree.basic;
 
 public class Node {
+
+	// 序号
 	int iData;
+	// 数据值
 	double fData;
 	Node leftChild;
 	Node rightChild;
---
+++ app-algorithm/src/main/java/com/tree/tree2/Tree2.java
@@ -0,0 +1,17 @@
+package com.tree.tree2;
+
+/**
+ * @author gaoquan on 2017/7/18.
+ */
+public class Tree2 {
+
+	private Node root;
+
+	public void build(){
+		Node current = root;
+
+
+
+
+	}
+}

1a05247c52b1f8dd7566ff51da7065e80699f5d4
fekefe <fekefe@126.com>
2017-08-02 08:59:39 +0800

    字符串倒序

---
+++ ggh-test/src/main/java/com/ggh/string/StringSortTest.java
@@ -0,0 +1,30 @@
+package com.ggh.string;
+
+import org.junit.Test;
+
+/**
+ * @author gaoquan on 2017/7/31.
+ */
+public class StringSortTest {
+
+	@Test
+	public void Test1() {
+		String str = "abcdefg";
+		int length = str.length() - 1;
+		String t = "";
+		for (; length >= 0; length--)
+			t += str.charAt(length);
+		System.out.println(t);
+	}
+
+	@Test
+	public void Test2() {
+		String str = "abcdefg";
+		char[] chars = str.toCharArray();
+		int length = chars.length - 1;
+		String t = "";
+		for (; length >= 0; length--)
+			t += chars[length];
+		System.out.println(t);
+	}
+}

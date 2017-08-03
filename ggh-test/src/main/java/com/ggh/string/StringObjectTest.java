00e6eee7daf8986c74a6e60ed18ec8039ad6eb6c
fekefe <fekefe@126.com>
2017-08-03 13:46:52 +0800

    字符串对象对比

--- ggh-test/src/main/java/com/ggh/string/StringObjectTest.java
+++ ggh-test/src/main/java/com/ggh/string/StringObjectTest.java
@@ -4,17 +4,37 @@ import org.junit.Test;
 
 /**
  * 对象比较
- * 
+ * <p>
  * Created by gaoquan on 2017/3/25.
  */
 public class StringObjectTest {
 
+	int t;
+
 	@Test
 	public void ObjectCountTest() {
-		String a, b, c;
-		a = "test";
-		b = a;
-		c = b;
-		System.out.println(a == c);
+		String a1, b1, c1;
+		a1 = "test";
+		b1 = a1;
+		c1 = b1;
+
+		System.out.println(a1 == c1);
+
+		String a = "abcde";
+		String b = "abc" + "de";
+		String e = new String("abcde");
+		final String d = "abcde";
+		String f = "abc" + new String("de");
+		String g = "de";
+		String h = "abc" + g;
+
+		System.out.println(a == b);
+		System.out.println(a == e);
+		System.out.println(a == d);
+		System.out.println(a == f);
+		System.out.println(a == h);
+		System.out.println(t);
 	}
+
+
 }

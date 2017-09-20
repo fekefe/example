3ff048b55285f176aefe6dbed788e87b169cb490
fekefe <fekefe@126.com>
2017-09-20 15:20:39 +0800

    sort

--- app-algorithm/src/main/java/com/sort/sortbasic/ArrayBub.java
+++ app-algorithm/src/main/java/com/sort/sortbasic/ArrayBub.java
@@ -25,6 +25,34 @@ public class ArrayBub {
 		ArrayBub.bubbleSort(mn.theArray);
 		mn.display(mn.theArray);
 	}
+/*
+		7 8 6 2 1 	7 8 6 2 1
+		7 6 8 2 1 	7 6 8 2 1
+		7 6 2 8 1 	7 6 2 8 1
+		7 6 2 1 8 	7 6 2 1 8
+
+		6 7 2 1 8 	6 7 2 1 8
+		6 2 7 1 8 	6 2 7 1 8
+		6 2 1 7 8 	6 2 1 7 8
+		6 2 1 7 8
+
+		2 6 1 7 8 	2 6 1 7 8
+		2 1 6 7 8 	2 1 6 7 8
+		2 1 6 7 8
+		2 1 6 7 8
+
+		1 2 6 7 8 	1 2 6 7 8
+		1 2 6 7 8
+		1 2 6 7 8
+		1 2 6 7 8
+	循环16次，交换10次	循环10次，交换10次
+*/
+	@Test
+	public void test() {
+		long[] a = {8, 7, 6, 2, 1};
+		// long[] a = {7, 8, 2, 1, 6};
+		ArrayBub.bubbleSort2(a);
+	}
 
 	/**
 	 * 冒泡排序
@@ -34,15 +62,20 @@ public class ArrayBub {
 	 * @param a the a
 	 */
 	public static void bubbleSort(long[] a) {
+		int forTimes = 0;
+		int swapTimes = 0;
 		int nElems = a.length;
 		for (int i = nElems - 1; i >= 1; i--) {
 			for (int j = 0; j < i; j++) {
+				forTimes++;
 				if (a[j] > a[j + 1]) {
 					ArrayUtils.swap(a, j, j + 1);
+					swapTimes++;
 				}
 				MakeNum.display1(a);
 			}
 		}
+		System.out.println("循环" + forTimes + "次，交换" + swapTimes + "次");
 	}
 
 	/**
@@ -53,22 +86,19 @@ public class ArrayBub {
 	 */
 	public static void bubbleSort2(long[] a) {
 		// int out, in;
+		int forTimes = 0;
+		int swapTimes = 0;
 		int nElems = a.length;
 		for (int i = 0; i < nElems - 1; i++) {
 			for (int j = 0; j < nElems - 1; j++) {
+				forTimes++;
 				if (a[j] > a[j + 1]) {
 					ArrayUtils.swap(a, j, j + 1);
+					swapTimes++;
 				}
 				MakeNum.display1(a);
 			}
 		}
+		System.out.println("循环" + forTimes + "次，交换" + swapTimes + "次");
 	}
-
-	@Test
-	public void test() {
-		// long[] a = {8, 7, 6, 2, 1};
-		long[] a = {7, 8, 2, 1, 6};
-		ArrayBub.bubbleSort(a);
-	}
-
 }
--- app-algorithm/src/main/java/com/sort/sortbasic/ArraySel.java
+++ app-algorithm/src/main/java/com/sort/sortbasic/ArraySel.java
@@ -2,6 +2,7 @@ package com.sort.sortbasic;
 
 import com.util.ArrayUtils;
 import com.util.MakeNum;
+import org.junit.Test;
 
 /**
  * 选择排序
@@ -20,6 +21,34 @@ public class ArraySel {
 		ArraySel.selectionSort2(mn.theArray);
 		mn.display(mn.theArray);
 	}
+	/*
+7 8 6 2 1 	1 7 6 2 8
+6 8 7 2 1
+2 8 7 6 1
+1 8 7 6 2
+
+1 7 8 6 2 	1 2 6 7 8
+1 6 8 7 2
+1 2 8 7 6
+
+
+1 2 7 8 6 	1 2 6 7 8
+1 2 6 8 7
+
+
+
+1 2 6 7 8 	1 2 6 7 8
+
+
+
+循环10次，交换10次	循环10次，交换2次
+	*/
+	@Test
+	public void test() {
+		long[] a = {8, 7, 6, 2, 1};
+		// long[] a = {7, 8, 2, 1, 6};
+		ArraySel.selectionSort2(a);
+	}
 
 	/**
 	 * 选择排序
@@ -28,19 +57,25 @@ public class ArraySel {
 	 * @param a
 	 */
 	public static void selectionSort(long[] a) {
+		int forTimes = 0;
+		int swapTimes = 0;
 		int min;
 		int nElems = a.length;
 		for (int i = 0; i < nElems - 1; i++) {
 			min = i;
 			for (int j = i + 1; j < nElems; j++) {
+				forTimes++;
 				if (a[j] < a[min]) {
 					min = j;
 				}
 			}
 			if (i != min) {
 				ArrayUtils.swap(a, i, min);
+				swapTimes++;
 			}
+			MakeNum.display1(a);
 		}
+		System.out.println("循环" + forTimes + "次，交换" + swapTimes + "次");
 	}
 
 	/**
@@ -50,13 +85,19 @@ public class ArraySel {
 	 * @param a
 	 */
 	public static void selectionSort2(long[] a) {
+		int forTimes = 0;
+		int swapTimes = 0;
 		int nElems = a.length;
 		for (int i = 0; i < nElems - 1; i++) {
 			for (int j = i + 1; j < nElems; j++) {
+				forTimes++;
 				if (a[i] > a[j]) {
 					ArrayUtils.swap(a, i, j);
+					swapTimes++;
 				}
+				MakeNum.display1(a);
 			}
 		}
+		System.out.println("循环" + forTimes + "次，交换" + swapTimes + "次");
 	}
 }

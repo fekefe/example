045438b66eaa82f4969c33f3fc44908b75af20ff
fekefe <fekefe@126.com>
2017-12-07 13:24:58 +0800

    socket

---
+++ ggh-test/src/main/java/com/ggh/net/socket/EchoTest.java
@@ -0,0 +1,61 @@
+package com.ggh.net.socket;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ */
+public class EchoTest {
+
+	private final static String hostName = "127.0.0.1";
+	private final static String port = "8090";
+
+	@Test
+	public void server() {
+		int portNumber = Integer.parseInt(port);
+		try (
+				ServerSocket serverSocket = new ServerSocket(portNumber);
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+		) {
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				out.println(inputLine + " server add to ");
+			}
+		} catch (IOException e) {
+			System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void main(String[] args) {
+		int portNumber = Integer.parseInt(port);
+		try (
+				Socket echoSocket = new Socket(hostName, portNumber);
+				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+		) {
+			String userInput;
+			while ((userInput = stdIn.readLine()) != null) {
+				out.println(userInput);
+				System.out.println("echo: " + in.readLine());
+			}
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host " + hostName);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to " + hostName);
+			System.exit(1);
+		}
+	}
+
+}

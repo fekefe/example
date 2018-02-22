112e92ba9e413863bb0727f31f56309875366ce1
fekefe <fekefe@126.com>
2018-02-22 10:55:35 +0800

    Long Object

---
+++ ggh-test/src/main/java/com/ggh/numtest/LongTest.java
@@ -0,0 +1,20 @@
+package com.ggh.numtest;
+
+import org.junit.Test;
+
+/**
+ * @author DEPT RD4 gaoquan(gaoquan@dvt.dvt.com) on 2018/2/12
+ */
+public class LongTest {
+	
+	@Test
+	public void test1() {
+		Integer a = 100;
+		Integer b = 100;
+		System.out.println(a == b);
+		
+		Integer c = 200;
+		Integer d = 200;
+		System.out.println(c == d);
+	}
+}

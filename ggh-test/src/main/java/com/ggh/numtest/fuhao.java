5a1a430c34ab7a7921191abf7db9e8dd8be78f2a
fekefe <fekefe@126.com>
2018-07-02 17:25:47 +0800

    test push

---
+++ ggh-test/src/main/java/com/ggh/numtest/fuhao.java
@@ -0,0 +1,35 @@
+package com.ggh.numtest;
+
+/**
+ * @author DEPT RD4 gaoquan(gaoquan@dvt.dvt.com) on 2018/4/13
+ */
+public class fuhao {
+	
+	public static void main(String[] args) {
+		
+		int a = 1;
+		a |= 1;
+		System.out.println(a);
+		
+		a = 0;
+		a |= 1;
+		System.out.println(a);
+		
+		a = 2;
+		a |= 3;
+		System.out.println(a);
+		
+		a = 7;
+		a |= 9;
+		System.out.println(a);
+		
+		a = 15;
+		a |= 10;
+		System.out.println(a);
+		
+		a = 30;
+		a |= 10;
+		System.out.println(a);
+		
+	}
+}

0ecbeb0ae983dcbb90bc64a604073ce2d110f737
fekefe <fekefe@126.com>
2018-08-17 10:33:41 +0800

    修改名称

--- app-algorithm/src/main/java/com/gof/command/ClientDemo.java
+++ app-algorithm/src/main/java/com/gof/command/ClientDemo.java
@@ -7,9 +7,12 @@ import com.gof.command.impl.ActionStopCommandImpl;
 /**
  * 命令模式
  * 客户端
+ * <br>
+ * 在软件系统中，行为请求者与行为实现者通常是一种紧耦合的关系
+ * 但某些场合，比如需要对行为进行记录、撤销或重做、事务等处理时，这种无法抵御变化的紧耦合的设计就不太合适。
  * @author gaoquan on 2018/8/16
  */
-public class Client {
+public class ClientDemo {
 	
 	public static void main(String[] args) {
 		Receiver receiver = new Receiver();

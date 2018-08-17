eee2a0ac50a8fab0dc9e4e566ff31cad1381f102
fekefe <fekefe@126.com>
2018-08-17 09:07:51 +0800

    策略模式

--- app-algorithm/src/main/java/com/gof/strategy/Strategy.java
+++ app-algorithm/src/main/java/com/gof/strategy/Strategy.java
@@ -4,5 +4,5 @@ package com.gof.strategy;
  * @author DEPT RD4 gaoquan(gaoquan@dvt.dvt.com) on 2018/2/11
  */
 public interface Strategy {
-	public int doOperation(int num1, int num2);
+	int doOperation(int num1, int num2);
 }
--- app-algorithm/src/main/java/com/gof/strategy/StrategyPatternDemo.java
+++ app-algorithm/src/main/java/com/gof/strategy/StrategyPatternDemo.java
@@ -1,14 +1,15 @@
 package com.gof.strategy;
 
 /**
- * @author DEPT RD4 gaoquan(gaoquan@dvt.dvt.com) on 2018/2/11
+ * 策略模式
+ * 定义一系列的算法，把它们一个个封装起来, 并且使它们可相互替换。
  */
 public class StrategyPatternDemo {
 	public static void main(String[] args) {
 		Context context = new Context(new OperationAdd());
 		System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
 		
-		context = new Context(new OperationSubstract());
+		context = new Context(new OperationSubtract());
 		System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
 		
 		context = new Context(new OperationMultiply());
@@ -18,7 +19,7 @@ public class StrategyPatternDemo {
 		Strategy strategy = new OperationAdd();
 		System.out.println("10 + 5 = " + strategy.doOperation(10, 5));
 		
-		strategy = new OperationSubstract();
+		strategy = new OperationSubtract();
 		System.out.println("10 - 5 = " + strategy.doOperation(10, 5));
 		
 		strategy = new OperationMultiply();

bf28ef5f49e1b3f3691b861ef15f5a159d2c7f4f
fekefe <fekefe@126.com>
2018-08-17 10:26:47 +0800

    修改名称

--- app-algorithm/src/main/java/com/algorithm/offer/Class12.java
+++ app-algorithm/src/main/java/com/algorithm/offer/Class12.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 /**
  * 打印1到最大的n位数
  * 题目：打印1到最大的n位数，输入数字n,按顺序打印输出从1到最大的n位十进制数，比如输入3，打印从1到999.
- * @author DEPT RD4 gaoquan(gaoquan@dvt.dvt.com) on 2018/1/25
+ * @author gaoquan on 2018/1/25
  */
 public class Class12 {
 	

707792d8231e68eb78ace0758c37b6506937f544
fekefe <fekefe@126.com>
2018-08-20 13:35:11 +0800

    Long object

--- ggh-test/src/main/java/com/ggh/numtest/LongTest.java
+++ ggh-test/src/main/java/com/ggh/numtest/LongTest.java
@@ -9,12 +9,35 @@ public class LongTest {
 	
 	@Test
 	public void test1() {
-		Integer a = 100;
-		Integer b = 100;
+		System.out.println("-129:");
+		Integer a = -129;
+		Integer b = -129;
 		System.out.println(a == b);
+		System.out.println(a.intValue() == b.intValue());
+		System.out.println();
 		
-		Integer c = 200;
-		Integer d = 200;
+		System.out.println("-128:");
+		Integer c = -128;
+		Integer d = -128;
 		System.out.println(c == d);
+		System.out.println(c.intValue() == d.intValue());
+		System.out.println();
+		
+		System.out.println("127:");
+		Integer e = 127;
+		Integer f = 127;
+		System.out.println(e == f);
+		System.out.println(e.intValue() == f.intValue());
+		System.out.println();
+		
+		System.out.println("128:");
+		Integer g = 128;
+		Integer h = 128;
+		System.out.println(g == h);
+		System.out.println(g.intValue() == h.intValue());
+		System.out.println();
+		
+		
+		
 	}
 }

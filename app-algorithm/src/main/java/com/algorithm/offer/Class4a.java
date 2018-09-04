bc251ad247e12d6447f94b6071dc44b86b5fccd7
fekefe <fekefe@126.com>
2018-09-04 17:20:22 +0800

    rename

--- app-algorithm/src/main/java/com/algorithm/offer/Class4a.java
+++ app-algorithm/src/main/java/com/algorithm/offer/Class4a.java
@@ -13,45 +13,47 @@ public class Class4a {
 		int[] array1 = {4, 5, 6, 7, 8, 10};
 		int[] array2 = {1, 2, 3, 4, 5};
 		
-		for (int i : temp(array1, array2)) {
+		int[] mergeArray = mergeMethod(array1, array2);
+		
+		for (int i : mergeArray) {
 			System.out.println(i);
 		}
 	}
 	
 	
-	private static int[] temp(int[] array1, int[] array2) {
-		int[] temp = new int[array1.length + array2.length];
+	private static int[] mergeMethod(int[] array1, int[] array2) {
+		int[] mergeArray = new int[array1.length + array2.length];
 		int i = array1.length - 1;
 		int j = array2.length - 1;
 		int n = array1.length + array2.length - 1;
 		while (n >= 0) {
 			if (i >= 0 && j >= 0) {
 				if (array1[i] > array2[j]) {
-					temp[n] = array1[i];
+					mergeArray[n] = array1[i];
 					n--;
 					i--;
 				} else if (array1[i] < array2[j]) {
-					temp[n] = array2[j];
+					mergeArray[n] = array2[j];
 					n--;
 					j--;
 				} else if (array1[i] == array2[j]) {
-					temp[n] = array1[i];
-					temp[n - 1] = array2[j];
+					mergeArray[n] = array1[i];
+					mergeArray[n - 1] = array2[j];
 					n = n - 2;
 					i--;
 					j--;
 				}
 			} else {
 				if (i >= 0) {
-					temp[n--] = array1[i--];
+					mergeArray[n--] = array1[i--];
 				}
 				
 				if (j >= 0) {
-					temp[n--] = array2[j--];
+					mergeArray[n--] = array2[j--];
 				}
 			}
 		}
 		
-		return temp;
+		return mergeArray;
 	}
 }
